7846f9cc421fc1de72fc46ce1e6617c17eb64948
Adam Becker <amkbecker@gmail.com>
2021-06-16 19:31:35 +0200

    Update js/script.js-2

--- js/script.js
+++ js/script.js
@@ -67,45 +67,35 @@ function generateTitleLinks(){
     
     const articles = document.querySelectorAll(optArticleSelector);
     
-    let html = '';
-    
     for(let article of articles) {    
         
         /* get the article id */
 
         const articleId = article.getAttribute("id");
-        console.log(articleId);
-
+        
         /* find the title element */
 
         /* get the title from the title element */
 
         const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-        console.log(articleTitle);
-
+        
         /* create HTML of the link */
 
         const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-        console.log(linkHTML);
-
+        
         /* insert link into titleList */
 
         titleList.insertAdjacentHTML('beforeend',linkHTML);
-        html = html + linkHTML;
-        console.log(html);
         }
     
-        titleList.innerHTML = html;
-    
-        const links = document.querySelectorAll('.titles a');
-        console.log(links);
-    
-        for(let link of links){
-        link.addEventListener('click', titleClickHandler);
-        }
+    const links = document.querySelectorAll('.titles a');
+        
+    for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+    }
 
     }
 
-    generateTitleLinks();
+generateTitleLinks();
     
 }
